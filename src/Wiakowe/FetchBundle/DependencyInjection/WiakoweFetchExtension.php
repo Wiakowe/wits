fcacfc8b1866f81f0cff7e432ae60b2f8490cc34
Roger Llopart Pla <lumbendil@gmail.com>
2012-09-14 13:55:24 +0200

    Created the WiakoweFetchBundle.

---
+++ src/Wiakowe/FetchBundle/DependencyInjection/WiakoweFetchExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wiakowe\FetchBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class WiakoweFetchExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $this->createClients($config, $container);
+    }
+
+    protected function createClients(array $config, ContainerBuilder $container)
+    {
+        $clientPrefix = 'wiakowe_fetch.clients.';
+
+        foreach ($config['clients'] as $clientName => $clientConfig) {
+            $definition = new Definition('Fetch\Server', array($clientConfig['host'], $clientConfig['port']));
+            $definition->addMethodCall('setAuthentication', array($clientConfig['user'], $clientConfig['password']));
+
+            $container->setDefinition($clientPrefix . $clientName, $definition);
+        }
+
+        $container->setAlias('wiakowe_fetch.client', $clientPrefix . $config['default_client']);
+    }
+}
---
+++ src/Wiakowe/FetchBundle/WiakoweFetchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wiakowe\FetchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WiakoweFetchBundle extends Bundle
+{
+}
