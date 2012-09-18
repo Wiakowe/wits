ce25e5bd43cf5179b1930def105a557a244a05e9
Aitor Suso <patxi1980@gmail.com>
2012-09-18 19:16:23 +0200

    #10 Added styles to almost every page, the sass styles, and the gravatar bundle to put images for the users.

--- src/Wits/IssueBundle/Repository/IssueRepository.php
+++ src/Wits/IssueBundle/Repository/IssueRepository.php
@@ -5,6 +5,7 @@ namespace Wits\IssueBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 use Wits\IssueBundle\Entity\Issue;
 use Wits\ProjectBundle\Entity\Project;
+use Wits\HelperBundle\Util\ArrayUtil;
 
 class IssueRepository extends EntityRepository
 {
@@ -19,5 +20,29 @@ class IssueRepository extends EntityRepository
                 ->getQuery();
 
         return $query->getOneOrNullResult();
+    }
+
+    public function getIssuesByType(Project $project)
+    {
+        $results =  $this->createQueryBuilder('i')
+            ->select('i.status, count(i.status) AS cnt')
+            ->where('i.project = :project')
+            ->setParameter('project', $project->getId())
+            ->groupBy('i.status')
+            ->orderBy('i.status')
+            ->getQuery()
+            ->getArrayResult();
+
+        return ArrayUtil::setElementAsKeyValue($results, 'status', 'cnt');
+    }
+
+    public function getNumberOfIssuesByProject(Project $project)
+    {
+        return $this->createQueryBuilder('i')
+            ->select('count(i.id) AS cnt')
+            ->where('i.project = :project')
+            ->setParameter('project', $project->getId())
+            ->getQuery()
+            ->getSingleResult();
     }
 }
