2579ff04340bab80102403f5b8e89b5c1bf820d1
Roger Llopart Pla <lumbendil@gmail.com>
2012-09-26 11:12:41 +0200

    #17 Added markdown parser.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Wiakowe\FetchBundle\WiakoweFetchBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
             new Wiakowe\DoctrineTablePrefixBundle\WiakoweDoctrineTablePrefixBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
